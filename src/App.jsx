6662f39e7c55ead4d36dee746ec5dcc0d61657d2
chauhanorganicstore <vijaychauhan200104@gmail.com>
2025-11-29 22:58:02 +0530

    fix(build): remove react-router-dom usage for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Admin from "./pages/Admin";
-
+import ProductList from "./components/ProductList"; // ??? ??? component ??
 export default function App(){
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/admin" element={<Admin/>} />
-      </Routes>
-    </BrowserRouter>
+    <div style={{padding:20}}>
+      <h1>Chauhan Organics</h1>
+      <ProductList />
+    </div>
   );
 }
