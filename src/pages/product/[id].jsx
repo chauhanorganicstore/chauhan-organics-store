7d6efe81421dd226069f5dd51dab6803214146a4
chauhanorganicstore <vijaychauhan200104@gmail.com>
2025-11-30 11:48:51 +0530

    add ProductPage ([id].jsx) and MainLayout via powershell

---
+++ src/pages/product/[id].jsx
@@ -0,0 +1,15 @@
+﻿import React, { useState } from "react";
+import MainLayout from "../../layouts/MainLayout";
+
+export default function ProductPage({ product, products }) {
+  const [isAdmin] = useState(false);
+
+  return (
+    <MainLayout isAdmin={isAdmin} product={product} products={products}>
+      <article>
+        <h1>{product?.title}</h1>
+        <p>{product?.description}</p>
+      </article>
+    </MainLayout>
+  );
+}
